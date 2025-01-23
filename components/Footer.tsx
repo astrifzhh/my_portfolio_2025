86d9ab96d2ba0381d5fefdb0417c1366a4129876
unknown <astrifauziah287@gmail.com>
2025-01-24 01:16:03 +0700

    Fixing bug change footer, make Profile.id as String

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -35,10 +35,16 @@ const Footer = () => {
               href={profile.link}
               target="_blank"
               rel="noopener noreferrer"
-              key={profile.id}
+              // key={profile.id}
+              key={String(profile.id)}
               className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 dark:bg-black-200 bg-white-200 rounded-full"
             >
-              <img src={profile.img} alt={profile.id} width="20" height="20" />
+              <img
+                src={profile.img}
+                alt={String(profile.id)}
+                width="20"
+                height="20"
+              />
             </a>
           ))}
         </div>
