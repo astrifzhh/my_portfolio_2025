f815a77c658a9b3d0b4d6fd4f2b42b49babee0f3
unknown <astrifauziah287@gmail.com>
2025-01-24 00:44:51 +0700

    Fixing bug 'img'

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import { InfiniteMovingCards } from "./ui/InfiniteMovingCard";
 import { companies, testimonials } from "@/data";
@@ -20,7 +21,7 @@ const Clients = () => {
       </div>
 
       <div className="flex flex-wrap items-center justify-center gap-4 md:gap-16 max-lg:mt-10">
-        {companies.map(({ id, img, name, nameImg }) => (
+        {companies.map(({ id, name, nameImg }) => (
           <div key={id} className="flex md:max-w-60 max-w-32 gap-2">
             {/* <img src={img} alt={name} className="md:w-10 w-5" /> */}
             <img src={nameImg} alt={name} className="md:w-24 w-20" />
